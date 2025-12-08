b84644a30cd6febff5283c76f4897181a98fb4f6
Liam Witterick <liam.witterick@ecoonline.com>
2025-12-08 09:39:01 +0000

    Release version 2.2.0

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 # Build variables
 BINARY_NAME=autoengineer
-VERSION=2.1.0
+VERSION=2.2.0
 BUILD_DIR=dist
 GO_CMD=go
 GO_BUILD=$(GO_CMD) build
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-VERSION="2.1.0"
+VERSION="2.2.0"
 INSTALL_DIR="${INSTALL_DIR:-$HOME/.local/bin}"
 GITHUB_REPO="liam-witterick/autoengineer"
 BINARY_NAME="autoengineer"
